153468e09410143f1914d433f4e8a0ff833552df
Jeffrey Lonzanida <111095026+ifejinity@users.noreply.github.com>
2023-07-28 00:36:34 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const resultDiv = document.querySelector('#result');
 function fetchData() {
 //clear the current displayed data
-    resultDiv.innerHTML = '';
-  // Replace 'YOUR_API_URL' with the actual URL of the API you want to fetch data from
+resultDiv.innerHTML = '';
+
   let word = $('#word').val();
   const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
@@ -12,7 +12,7 @@ function fetchData() {
     dataType: 'json',
     success: function (data) {
         var myData = [];
-        // Handle the data received from the API
+
         for (let a = 0; a < data.length; a++) {
             for (let b = 0; b < data[a].meanings.length; b++) {
                 for (let c = 0; c < data[a].meanings[b].definitions.length; c++) {
@@ -32,7 +32,7 @@ function fetchData() {
 }
 
 function displayData(data) {
-  // Example: Display the received data on the page
+
   for(a = 0; a < data.length; a++){
     resultDiv.innerHTML += `<li>${data[a]}</li>`;
   }
